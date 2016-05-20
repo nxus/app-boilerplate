15137cab4211566a8d65d02d7b3ebb10082f4738
Mike Reich <mjreich@gmail.com>
2016-05-20 06:59:11 -0700

    First version of boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# app-boilerplate
+# nxus-app-boilerplate
+
+## Installation
+
+	> git clone git@github.com:nxus/app-boilerplate.git && rm -rf .git
+
+## Starting
+
+	> npm start
+
+## Testing
+
+	> npm test
+
+## Usage
+
+Add your modules in the `modules` folder
---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+* @Author: mike
+* @Date:   2016-05-20 06:56:14
+* @Last Modified 2016-05-20eich
+* @Last Modified time: 2016-05-20 06:57:18
+*/
+
+'use strict';
+
+var Application = require('nxus-core').Application
+
+var myApp = new Application()
+
+myApp.start()
