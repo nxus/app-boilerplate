0261b25ef644a7dc037d7f59f791d6846b48b2bd
Mike Reich <mjreich@gmail.com>
2016-08-02 13:02:43 -0700

    Fixed port and added ignore paths to app config

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /*
 * @Author: mike
 * @Date:   2016-05-20 06:56:14
-* @Last Modified 2016-05-20eich
-* @Last Modified time: 2016-05-20 06:57:18
+* @Last Modified 2016-08-02
+* @Last Modified time: 2016-08-02 13:02:14
 */
 
 'use strict';
 
 var Application = require('nxus-core').Application
 
-var myApp = new Application()
+var myApp = new Application({ignore: ['.tmp', new RegExp("\.ejs")]})
 
 myApp.start()
