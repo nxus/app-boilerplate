3b545649f69c727486bcccbc6723102cac454631
Mike Reich <mjreich@gmail.com>
2016-08-25 10:45:13 -0700

    Updated logging run task

--- modules/homepage/index.js
+++ modules/homepage/index.js
@@ -2,7 +2,7 @@
 * @Author: mike
 * @Date:   2016-05-19 21:26:41
 * @Last Modified 2016-08-25
-* @Last Modified time: 2016-08-25 10:36:40
+* @Last Modified time: 2016-08-25 10:41:01
 */
 
 'use strict';
@@ -13,6 +13,6 @@ export default class Homepage extends NxusModule {
   constructor(app) {
     super(app)
 
-    this.log('Loading Homepage')
+    this.log.debug('Loading Homepage')
   }
 }
