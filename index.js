1c5e7a519adab61b82f30afbc1c9dc4dea7074e1
Mike Reich <mjreich@gmail.com>
2016-08-25 10:37:11 -0700

    Updated index to use compiled version in production. Update homepage to use new module 4.0 syntax

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 /*
 * @Author: mike
 * @Date:   2016-05-20 06:56:14
-* @Last Modified 2016-08-02
-* @Last Modified time: 2016-08-02 13:02:14
+* @Last Modified 2016-08-25
+* @Last Modified time: 2016-08-25 10:34:50
 */
 
 'use strict';
 
+if(process.env.NODE_ENV != 'production') {
+  require('babel-register');
+  var modulesDir = __dirname+"/modules"
+} else {
+  var modulesDir = __dirname+"/lib"
+  var g = require('idle-gc');
+  g.start();
+} 
+
 var Application = require('nxus-core').Application
 
-var myApp = new Application({ignore: ['.tmp', new RegExp("\.ejs")]})
+let opts = {
+  ignore: ['.tmp', 'modules/app/js', 'modules/countries/js', new RegExp('\.ejs'), new RegExp('\.css')],
+  modulesDir
+}
+
+var myApp = new Application(opts);
 
 myApp.start()
+
+module.exports = myApp;
--- modules/homepage/index.js
+++ modules/homepage/index.js
@@ -1,17 +1,18 @@
 /*
 * @Author: mike
 * @Date:   2016-05-19 21:26:41
-* @Last Modified 2016-05-19
-* @Last Modified time: 2016-05-19 21:49:43
+* @Last Modified 2016-08-25
+* @Last Modified time: 2016-08-25 10:36:40
 */
 
 'use strict';
 
-module.exports = function(app) {
-  app.get('router').route('/', function(req, res) {
-    app.get('templater').render('page', {
-      title: 'Hello',
-      content: 'World'
-    }).then(res.send.bind(res))
-  })
+import {application, NxusModule} from 'nxus-core'
+
+export default class Homepage extends NxusModule {
+  constructor(app) {
+    super(app)
+
+    this.log('Loading Homepage')
+  }
 }
