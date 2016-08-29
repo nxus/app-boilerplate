101242043e4cea13a4a82240db8a98130a980af5
Luke Opperman <luke@opperman.net>
2016-08-29 12:38:13 -0400

    Make use of templater

--- modules/homepage/index.js
+++ modules/homepage/index.js
@@ -11,14 +11,24 @@ import {application, NxusModule} from 'nxus-core'
 
 import {router} from 'nxus-router'
 
+import {templater} from 'nxus-templater'
+
 export default class Homepage extends NxusModule {
   constructor(app) {
     super(app)
 
     this.log.debug('Loading Homepage')
 
-    router.route("/", (req, res) => {
+    router.route("/hi", (req, res) => {
       res.send("Hi")
     })
+
+    templater.templateDir(__dirname+"/templates", "page", "ejs")
+
+    router.route("/", ::this.homepage)
+  }
+
+  homepage (req, res) {
+    templater.render("homepage", {greeting: "Hi"}).then(::res.send)
   }
 }
