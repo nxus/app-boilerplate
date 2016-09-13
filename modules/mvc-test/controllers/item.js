d27ad3f37238173a3b3609d7445313fee3a6f8da
Luke Opperman <luke@opperman.net>
2016-09-13 12:53:37 -0400

    override example view controller

--- modules/mvc-test/controllers/item.js
+++ modules/mvc-test/controllers/item.js
@@ -2,8 +2,17 @@ import {ViewController} from 'nxus-web'
 
 export default class MVCTestItem extends ViewController {
 
-  get modelIdentity() {
-    return 'mvc_test_item'
+  get modelIdentity() { return 'mvc_test_item' }
+
+  get displayName() { return 'Items' } 
+
+  detail(req, res, query) {
+    return super.detail(req, res, query).then((ctx) => {
+      // let object = ctx.object
+      return {
+        custom: "Customizing",
+        ...ctx
+      }
+    })
   }
-  
 }
