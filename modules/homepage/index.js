f9f294da6e5b16d20f6d919c01a071b8ee604727
Mike Reich <mjreich@gmail.com>
2016-09-19 11:30:29 -0700

    Fixed issues with homepage deps

--- modules/homepage/index.js
+++ modules/homepage/index.js
@@ -1,8 +1,8 @@
 /*
 * @Author: mike
 * @Date:   2016-05-19 21:26:41
-* @Last Modified 2016-09-06
-* @Last Modified time: 2016-09-06 20:55:36
+* @Last Modified 2016-09-13
+* @Last Modified time: 2016-09-13 14:05:17
 */
 
 'use strict';
@@ -12,7 +12,7 @@ import {application, NxusModule} from 'nxus-core'
 import {router} from 'nxus-router'
 
 import {templater} from 'nxus-templater'
-// import {clientjs} from 'nxus-clientjs'
+import {clientjs} from 'nxus-clientjs'
 
 export default class Homepage extends NxusModule {
   constructor(app) {
@@ -28,11 +28,10 @@ export default class Homepage extends NxusModule {
 
     router.route("/", ::this.homepage)
 
-    // clientjs.includeScript('homepage', __dirname+"/client/component.js")
+    clientjs.includeScript('homepage', __dirname+"/client/component.js")
   }
 
   homepage (req, res) {
-    console.log('rendering')
     templater.render("homepage", {greeting: "Hi"}).then(::res.send)
   }
 }
